a6222be88242d4ee7db064666ee2cf66370d2d4a
Gabriel Augusto Campos Rodrigues <gcampos-@c1r6s4.42porto.com>
2023-05-05 18:01:08 +0100

    comecando bonus

---
+++ libft/Bonus/ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gcampos- <gcampos-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/04 14:08:07 by gcampos-          #+#    #+#             */
+/*   Updated: 2023/05/05 17:54:57 by gcampos-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = (t_list *)malloc(sizeof(t_list));
+	if (!new)
+		return (NULL);
+	new->content = content;
+	new->next = NULL;
+	return (new);
+}
